e467d9051f1bc008d0a99962a047958fe85a1c78
shatadalsamui <143749872+shatadalsamui@users.noreply.github.com>
2025-06-09 09:00:00 +0530

    Init

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function(){
+    return <div>
+        Signin
+        <input type={"text"}></input>
+        <input type={"text"}></input>
+        <button>Sign in</button>
+    </div>
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function(){
+    return <div>
+        Signup
+        <input type={"text"}></input>
+        <input type={"text"}></input>
+        <button>Sign up</button>
+    </div>
+}
